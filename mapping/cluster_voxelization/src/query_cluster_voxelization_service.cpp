498f8ae15540f45d3901b291c714adce5d5b20d0
veedee <veedee@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2009-07-01 10:47:47 +0000

    query

---
+++ mapping/cluster_voxelization/src/query_cluster_voxelization_service.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2009 Technische Universitaet Muenchen
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * $Id: query_cluster_voxelization_service.cpp 17089 2009-06-15 18:52:12Z veedee $
+ *
+ */
+#include "ros/ros.h"
+#include "ros/node.h"
+#include <perception_srvs/ClustersVoxels.h>
+
+using namespace perception_srvs;
+
+/* ---[ */
+int
+  main (int argc, char** argv)
+{
+  ros::init (argc, argv, "query_cluster_voxelization_service");
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<ClustersVoxels>("clusters_service");
+
+  ClustersVoxels srv;
+  if (client.call (srv))
+  {
+    ROS_INFO ("Service call successful.");
+  }
+  else
+  {
+    ROS_ERROR ("Failed to call service!");
+    return (-1);
+  }
+
+  return (0);
+}
