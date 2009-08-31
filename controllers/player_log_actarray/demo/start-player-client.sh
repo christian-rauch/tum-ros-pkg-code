916b04ae005d0dcef66974b849b775feabd99bf7
dejanpan <dejanpan@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2009-08-31 17:31:16 +0000

    fixes

--- controllers/player_log_actarray/demo/start-player-client.sh
+++ controllers/player_log_actarray/demo/start-player-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CUR_DIR=`pwd`
-./laser_sweep_client/sweep_laser_scan_client -i 1 -s 0 -e -90 -j 4 -f ${CUR_DIR}/demo.log
+./laser_sweep_client/sweep_laser_scan_client -i 1 -s -92 -e -109 -j 4 -l 1 -f ${CUR_DIR}/demo.log
