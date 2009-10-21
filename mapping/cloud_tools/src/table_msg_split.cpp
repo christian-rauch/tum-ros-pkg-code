f693d0fb5b13cfcf397dd0dd70b6ef1ce827a130
blodow <blodow@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2009-10-21 17:48:34 +0000

    tiny bug fix

--- mapping/cloud_tools/src/table_msg_split.cpp
+++ mapping/cloud_tools/src/table_msg_split.cpp
@@ -44,7 +44,7 @@ class TableMsgSplit
       {
         sensor_msgs::PointCloud pc;
         pc.header = table->header;
-	//        pc.points = table->objects[i].points.points;
+	      pc.points = table->objects[i].points.points;
         clusters_pub_.publish (pc);
       }
     }
