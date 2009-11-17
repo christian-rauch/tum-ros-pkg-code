271985d9030a467782ad0c07e548b06b094c1aab
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2009-11-17 10:49:11 +0000

    Author: Ingo Kresse <kresse@cs.tum.edu> Date:   Mon Nov 16 19:08:45 2009 +0100
    Adding nav_pcontroller to tum-rosâpkg
    
    nav_pcontroller is a simple local planner for an omnidirectional base.
    It uses lasers to keep a minium distance to it's environment.
    
    git-commit

---
+++ controllers/nav_pcontroller/src/BaseDistance_test.cc
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2009, Ingo Kresse <kresse@in.tum.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <vector>
+#include <algorithm> //random_shuffle
+#include <math.h>
+#include <stdio.h>
+
+#include <assert.h>
+
+#include "BaseDistance.h"
+
+bool distance_test()
+{
+  std::vector<Vector2> points;
+  Vector2 n;
+  double d, eps=0.00001;
+
+  BaseDistance tester;
+  tester.top_ = 0.5;
+  tester.bottom_ = -0.7;
+  tester.left_ = -0.3;
+  tester.right_ = 0.8;
+  tester.tolerance_ = 0.1;
+
+  // edge cases
+  points.push_back(Vector2(3.2f, 0.0f));
+  d = tester.distance(points, &n);
+  assert((d-2.4)<eps && n.x == 3.2f && n.y == 0.0f);
+
+  points.push_back(Vector2(0.0f, 2.8f));
+  d = tester.distance(points, &n);
+  assert((d-2.3 < eps) && n.x == 0.0f && n.y == 2.8f);
+
+  points.push_back(Vector2(0.0f, -2.9f));
+  d = tester.distance(points, &n);
+  assert((d-2.2 < eps) && n.x == 0.0f && n.y == -2.9f);
+
+  points.push_back(Vector2(-2.4f, 0.0f));
+  d = tester.distance(points, &n);
+  assert((d-2.1 < eps) && n.x == -2.4f && n.y == 0.0f);
+
+
+  // corner cases
+  points.push_back(Vector2(1.76f, 1.6f));
+  d = tester.distance(points, &n);
+  assert((d-1.46 < eps) && n.x == 1.76f && n.y == 1.6f);
+
+  points.push_back(Vector2(-1.18f, 1.55f));
+  d = tester.distance(points, &n);
+  assert((d-1.37 < eps) && n.x == -1.18f && n.y == 1.55f);
+
+  points.push_back(Vector2(-1.05f, -1.7f));
+  d = tester.distance(points, &n);
+  assert((d-1.25 < eps) && n.x == -1.05f && n.y == -1.7f);
+
+  points.push_back(Vector2(0.95f, -1.82f));
+  d = tester.distance(points, &n);
+  assert((d-1.13 < eps) && n.x == 0.95f && n.y == -1.82f);
+
+
+  // check if the order matters - it should not.
+  for(int i=0; i < 1000; i++) {
+    std::random_shuffle(points.begin(), points.end());
+    d = tester.distance(points, &n);
+    assert((d-1.13 < eps) && n.x == 0.95f && n.y == -1.82f);
+  }
+
+}
+
+int main()
+{
+  distance_test();
+}
---
+++ controllers/nav_pcontroller/src/speed_filter.cc
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2009, Ingo Kresse <kresse@in.tum.de>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+/**
+
+@mainpage
+
+@htmlinclude manifest.html
+
+<hr>
+
+@section topic ROS topics
+
+Subscribes to (name/type):
+- @b "tf_message" tf/tfMessage: robot's pose in the "map" frame
+- @b "goal" geometry_msgs/PoseStamped : goal for the robot.
+
+Publishes to (name / type):
+- @b "cmd_vel" geometry_msgs/Twist : velocity commands to robot
+- @b "state" nav_robot_actions/MoveBaseState : current planner state (e.g., goal reached, no path)
+
+@section parameters ROS parameters
+  - @b "xy_tolerance" (double) : Goal distance tolerance (how close the robot must be to the goal before stopping), default: 0.05 m
+  - @b "th_tolerance" (double) : Goal rotation tolerance (how close the robot must be to the goal before stopping), default: 0.05 rad
+  - @b "vel_lin_max" (double) : maximum linear velocity, default: 0.2 m/s
+  - @b "vel_ang_max" (double) : maximum angular velocity, default: 0.2 rad/s
+  - @b "acc_lin_max" (double) : maximum linear acceleration, default: 0.1 m/s^2
+  - @b "acc_ang_max" (double) : maximum angular acceleration, default: 0.1 rad/s^2
+  - @b "loop_rate" (int) : rate at which the control loop runs, default: 30 s^-1
+  - @b "p" (double) : P controller value, default: 1.0
+
+*/
+
+#include <unistd.h>
+#include <math.h>
+
+#include "ros/ros.h"
+
+
+#include <roslib/Time.h>
+#include "tf/transform_listener.h"
+
+
+// The messages that we'll use
+//#include <nav_robot_actions/MoveBaseState.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Twist.h>
+#include <std_msgs/String.h>
+
+#include <sensor_msgs/LaserScan.h>
+
+#include "BaseDistance.h"
+
+class SpeedFilter {
+private:
+  BaseDistance dist_control_;
+
+  ros::NodeHandle n_;
+  ros::Publisher  pub_vel_;
+  ros::Subscriber sub_vel_;
+
+  void input_vel(const geometry_msgs::Twist::ConstPtr& msg);
+  
+public:
+  SpeedFilter();
+};
+
+void SpeedFilter::input_vel(const geometry_msgs::Twist::ConstPtr& msg)
+{
+  geometry_msgs::Twist t = *msg;
+
+  dist_control_.compute_distance_keeping(&(t.linear.x), &(t.linear.y), &(t.angular.z));
+  pub_vel_.publish(t);
+}
+
+
+SpeedFilter::SpeedFilter() : n_("~")
+{
+  sub_vel_ =  n_.subscribe("/input_vel", 1, &SpeedFilter::input_vel, this);
+  pub_vel_ =  n_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+
+  //CHANGED
+  //  dist_control_.setFootprint(0.42, -0.3075, 0.3075, -0.42, 0.0);
+  dist_control_.setFootprint(0.309, -0.43, 0.43, -0.309, 0.0);
+}
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "speed_filter");
+
+  SpeedFilter sf;
+
+  ros::spin();
+
+  return 0;
+}
