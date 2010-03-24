9c950eb7a5ecf2cd6e52e8bc59fad7aee5663535
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-03-24 00:25:26 +0000

    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu> Date:   Wed Mar 24 01:15:59 2010 +0100
    commit 3422245943a60cb6d24ce414abeecf659539fc59
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Wed Mar 24 01:12:07 2010 +0100
    
        Added a description and a licence header.
    
    commit 4e4a6655912c32d0401da240e4e2cedc8d12ba10
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Wed Mar 24 00:56:25 2010 +0100
    
        renamed
    
    git-commit

---
+++ perception/face_detector_mono/src/facedetect.cpp
@@ -0,0 +1,222 @@
+/*
+ * Copyright (C) 2010 by Dejan Pangercic <dejan.pangercic@cs.tum.edu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ 
+#define CV_NO_BACKWARD_COMPATIBILITY
+//opencv
+#include "cv.h"
+#include "highgui.h"
+
+#include <iostream>
+#include <cstdio>
+//ros
+#include <ros/ros.h>
+#include <ros/node_handle.h>
+#include "sensor_msgs/Image.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/CvBridge.h"
+
+//yarp
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+//namespaces
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::sig::draw;
+using namespace yarp::sig::file;
+
+#ifdef _EiC
+#define WIN32
+#endif
+
+using namespace std;
+using namespace cv;
+
+//#define YARP
+
+class FaceDetect {
+
+public:
+  String cascade_name_;
+  String nested_cascade_name_;
+  Mat  image_;
+  String input_image_topic_, port_name_;
+  CascadeClassifier cascade_, nested_cascade_;
+  double scale_;
+  bool display_;
+  Network yarp_;
+  // Make a port for reading and writing images
+  BufferedPort<Bottle> port_;
+  FaceDetect(ros::NodeHandle &n) :
+    n_(n), it_(n_)
+  {
+    n_.param("cascade_name", cascade_name_, std::string(""));
+    n_.param("nested_cascade_name", nested_cascade_name_, std::string(""));
+    n_.param("scale", scale_, 1.5);
+    n_.param("input_image_topic", input_image_topic_, std::string("/yarp_to_ros_image/yarp_to_ros_image"));
+    n_.param("display", display_, false);
+    n_.param("port_name", port_name_, std::string("/icub_look_at_head"));
+
+    image_sub_ = it_.subscribe(input_image_topic_, 1, &FaceDetect::image_callback, this);
+
+    if (cascade_name_ == "" || nested_cascade_name_ == "")
+      ROS_ERROR("Classification files missing!");
+    if(display_)
+      cvNamedWindow( "result", 1 );
+#ifdef YARP    
+    ConstString yarp_port_name_(port_name_.c_str());
+    yarp_.init();
+    port_.open(yarp_port_name_);
+#endif 
+  }
+
+  ~FaceDetect()
+    {
+      if(display_)
+        cvDestroyWindow("result");
+    }
+
+  void image_callback(const sensor_msgs::ImageConstPtr& msg_ptr)
+  {
+    try
+    {
+      image_ = bridge_.imgMsgToCv(msg_ptr, "bgr8");
+    }
+    catch (sensor_msgs::CvBridgeException error)
+    {
+      ROS_ERROR("error");
+    }
+    
+    if( !nested_cascade_.load( nested_cascade_name_ ) )
+      cerr << "WARNING: Could not load classifier cascade for nested objects" << endl;
+    
+    if( !cascade_.load( cascade_name_ ) )
+    {
+      cerr << "ERROR: Could not load classifier cascade" << endl;
+      cerr << "Usage: facedetect [--cascade=\"<cascade_path>\"]\n"
+        "   [--nested-cascade[=\"nested_cascade_path\"]]\n"
+        "   [--scale[=<image scale>\n"
+        "   [filename|camera_index]\n" ;
+      //return -1;
+    }
+    
+    if( !image_.empty() )
+    {
+      detect_and_draw( image_, cascade_, nested_cascade_, scale_ );
+      waitKey(100);
+    }   
+    
+    // return 0;
+  }
+
+  void detect_and_draw( Mat& img,
+                      CascadeClassifier& cascade, CascadeClassifier& nestedCascade,
+                      double scale)
+  {
+    int i = 0;
+    double t = 0;
+    vector<Rect> faces;
+    const static Scalar colors[] =  { CV_RGB(0,0,255),
+                                      CV_RGB(0,128,255),
+                                      CV_RGB(0,255,255),
+                                      CV_RGB(0,255,0),
+                                      CV_RGB(255,128,0),
+                                      CV_RGB(255,255,0),
+                                      CV_RGB(255,0,0),
+                                      CV_RGB(255,0,255)} ;
+    Mat gray, smallImg( cvRound (img.rows/scale), cvRound(img.cols/scale), CV_8UC1 );
+    
+    cvtColor( img, gray, CV_BGR2GRAY );
+    resize( gray, smallImg, smallImg.size(), 0, 0, INTER_LINEAR );
+    equalizeHist( smallImg, smallImg );
+    
+    t = (double)cvGetTickCount();
+    cascade.detectMultiScale( smallImg, faces,
+                              1.1, 3, 0
+                              //|CV_HAAR_FIND_BIGGEST_OBJECT
+                              //|CV_HAAR_DO_ROUGH_SEARCH
+                              |CV_HAAR_SCALE_IMAGE
+                              ,
+                              Size(30, 30) );
+    t = (double)cvGetTickCount() - t;
+    printf( "detection time = %g ms\n", t/((double)cvGetTickFrequency()*1000.) );
+    for( vector<Rect>::const_iterator r = faces.begin(); r != faces.end(); r++, i++ )
+    {
+      Mat smallImgROI;
+      vector<Rect> nestedObjects;
+      Point center;
+      Scalar color = colors[i%8];
+      int radius;
+      center.x = cvRound((r->x + r->width*0.5)*scale);
+      center.y = cvRound((r->y + r->height*0.5)*scale);
+#ifdef YARP 
+     //send center coordinates to the icub head
+        send_face_center_to_yarp_port(center.x, center.y);
+#endif      
+      radius = cvRound((r->width + r->height)*0.25*scale);
+      circle( img, center, radius, color, 3, 8, 0 );
+      if( nestedCascade.empty() )
+        continue;
+      smallImgROI = smallImg(*r);
+      nestedCascade.detectMultiScale( smallImgROI, nestedObjects,
+                                      1.1, 2, 0
+                                      //|CV_HAAR_FIND_BIGGEST_OBJECT
+                                      //|CV_HAAR_DO_ROUGH_SEARCH
+                                      //|CV_HAAR_DO_CANNY_PRUNING
+                                      |CV_HAAR_SCALE_IMAGE
+                                      ,
+                                      Size(30, 30) );
+      for( vector<Rect>::const_iterator nr = nestedObjects.begin(); nr != nestedObjects.end(); nr++ )
+      {
+        center.x = cvRound((r->x + nr->x + nr->width*0.5)*scale);
+        center.y = cvRound((r->y + nr->y + nr->height*0.5)*scale);
+        
+        radius = cvRound((nr->width + nr->height)*0.25*scale);
+        circle( img, center, radius, color, 3, 8, 0 );
+      }
+    }  
+    if(display_)
+      cv::imshow( "result", img );    
+  }
+
+#ifdef YARP
+  void send_face_center_to_yarp_port(double x, double y)
+  {
+    Bottle& output = port_.prepare();
+    output.clear();
+    output.addDouble(x);
+    output.addDouble(y);
+    ROS_INFO("Writting center coordinates %s to port %s", output.toString().c_str(), port_name_.c_str());
+    port_.write();
+  }
+#endif
+protected:
+  
+  ros::NodeHandle n_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  sensor_msgs::CvBridge bridge_;
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "facedetect");
+  ros::NodeHandle n("~");
+  FaceDetect f(n);
+  ros::spin();
+  return 0;
+}
