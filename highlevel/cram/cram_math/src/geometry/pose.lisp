51f3bf4460ebb0b9da4c609dc927528c3bf6a017
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-04-05 00:40:20 +0000

    Author: Lorenz Moesenlechner <lorenz@willowgarage.com> Date:   Sun Apr 4 16:22:42 2010 -0700
    Reorganized CRAM. It's now a stack with several ros packages.
    
    git-commit

---
+++ highlevel/cram/cram_math/src/geometry/pose.lisp
@@ -0,0 +1,74 @@
+
+(in-package :cma)
+
+(defclass pose ()
+  ((position :initform (make-instance 'point) :initarg :position
+             :accessor pos)
+   (orientation :initform (make-instance 'quaternion) :initarg :orientation
+                :accessor orientation))
+  (:documentation "a three-dimensional pose"))
+
+(defun pose (&optional (position (point)) (orientation (quaternion)))
+  (make-instance 'pose
+                 :position position
+                 :orientation orientation))
+
+(defun pose-inverse (p)
+  (let ((q-inv (quaternion-inverse (orientation p))))
+    (make-instance 'pose
+                   :position (rotate-point (point-inverse (pos p)) q-inv)
+                   :orientation q-inv)))
+
+(defun pose-* (&rest poses)
+  "Compose poses by first rotating around the parent pose and then adding up the points.
+   Processes from right to left, i.e. the right-most transformation
+   pair of transformations is applied first."
+  (reduce (lambda (prev p)
+            (make-instance 'pose
+                           :position (point-+ (pos p) (rotate-point (pos prev) (orientation p)))
+                           :orientation (quaternion-* (orientation prev) (orientation p))))
+          (reverse poses)))
+
+(defun pose->matrix (pose)
+  "Constructs a homogenous matrix from pose and returns it as a 2D array.
+   Please note: to convert it to a 1D array, you can do the following:
+
+  (make-array (array-total-size x) 
+              :element-type (array-element-type x) 
+              :displaced-to x)"
+  (with-slots (position orientation) pose
+    (let ((result (make-array '(4 4) :initial-element 0.0)))
+      (with-slots (x y z w) orientation
+        (setf (aref result 0 0) (- 1 (* 2 y y) (* 2 z z))
+              (aref result 0 1) (- (* 2 x y) (* 2 z w))
+              (aref result 0 2) (+ (* 2 x z) (* 2 y w))
+              (aref result 1 0) (+ (* 2 x y) (* 2 z w))
+              (aref result 1 1) (- 1 (* 2 x x) (* 2 z z))
+              (aref result 1 2) (- (* 2 y z) (* 2 x w))
+              (aref result 2 0) (- (* 2 x y) (* 2 y w))
+              (aref result 2 1) (+ (* 2 y z) (* 2 x w))
+              (aref result 2 2) (- 1 (* 2 x x) (* 2 y y))))
+      (with-slots (x y z) position
+        (setf (aref result 0 3) x
+              (aref result 1 3) y
+              (aref result 2 3) z
+              (aref result 3 3) 1))
+      result)))
+
+(defun matrix->pose (matrix)
+  "Converts a homoenouse 4x4 matrix to a pose object."
+  (assert (typep matrix '(array t (4 4))))
+  (let ((rotation-submatrix
+         (make-array '(3 3)
+                     :initial-contents (loop for y from 0 below 3
+                                             collecting (loop for x from 0 below 3
+                                                              collecting (aref matrix y x))))))
+    (pose (matrix->quaternion rotation-submatrix)
+          (point :x (aref matrix 0 3)
+                 :y (aref matrix 1 3)
+                 :z (aref matrix 2 3)))))
+
+(defmethod print-object ((obj pose) strm)
+  (print-unreadable-object (obj strm :type t)
+    (with-slots (position orientation) obj
+      (format strm "~{~<~%   ~a~>~}" (list position orientation)))))
