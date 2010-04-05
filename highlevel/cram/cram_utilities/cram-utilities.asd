d907b08d0ce87ad8dbb8f1e0ae6f5e0ea8f72392
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-04-05 00:41:13 +0000

    Author: Lorenz Moesenlechner <lorenz@willowgarage.com> Date:   Sun Apr 4 16:22:42 2010 -0700
    Reorganized CRAM. It's now a stack with several ros packages.
    
    git-commit

---
+++ highlevel/cram/cram_utilities/cram-utilities.asd
@@ -0,0 +1,32 @@
+; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; Base: 10 -*-
+
+(asdf:defsystem cram-utilities
+  :name "cram-utilities"
+  :author "Lorenz Moesenlechner <moesenle@cs.tum.edu>"
+  :version "0.1"
+  :maintainer "Lorenz Moesenlechner <moesenle@cs.tum.edu>"
+  :licence "BSD"
+  :description "Pattern matching and other utilities."
+  :long-description "Provides basic pattern matching, binding
+                     handling, and other utilities used by other cram
+                     components."
+  :depends-on (alexandria
+               portable-threads)
+
+  :components
+  ((:module "src"
+            :components
+            ((:file "package")
+             (:file "utils")
+             (:file "lazy")
+             (:file "patmatch")
+             (:file "data-pool")
+             (:file "clos")
+             (:file "ts-queue")
+             (:file "time"))
+            :serial t)))
+
+(defmethod asdf:perform ((o asdf:test-op)
+                         (c (eql (asdf:find-system 'cram-utilities))))
+  (asdf:operate 'asdf:load-op 'cram-utilities-tests)
+  (asdf:operate 'asdf:test-op 'cram-utilities-tests))
---
+++ highlevel/cram/cram_utilities/src/ts-queue.lisp
@@ -0,0 +1,96 @@
+;;;
+;;; Copyright (c) 2009, Lorenz Moesenlechner <moesenle@cs.tum.edu>,
+;;;                     Nikolaus Demmel <demmeln@cs.tum.edu>
+;;; All rights reserved.
+;;; 
+;;; Redistribution and use in source and binary forms, with or without
+;;; modification, are permitted provided that the following conditions are met:
+;;; 
+;;;     * Redistributions of source code must retain the above copyright
+;;;       notice, this list of conditions and the following disclaimer.
+;;;     * Redistributions in binary form must reproduce the above copyright
+;;;       notice, this list of conditions and the following disclaimer in the
+;;;       documentation and/or other materials provided with the distribution.
+;;;     * Neither the name of Willow Garage, Inc. nor the names of its
+;;;       contributors may be used to endorse or promote products derived from
+;;;       this software without specific prior written permission.
+;;; 
+;;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+;;; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+;;; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+;;; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+;;; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+;;; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+;;; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+;;; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+;;; POSSIBILITY OF SUCH DAMAGE.
+;;;
+
+(in-package :cram-utilities)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Thread Safe Queue
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defclass ts-queue ()
+  ((head :initform nil :type list)
+   (tail :initform nil :type list)
+   (lock)
+   (name :initform "" :initarg :name :type string
+         :documentation "Name is mainly for debugging locks (whostate in with-lock-held)"))
+  (:documentation "Simple thread safe queue implemented as cons list."))
+
+(defmethod initialize-instance :after ((queue ts-queue) &key)
+  (with-slots (lock name) queue
+    (setf lock (make-recursive-lock :name (format nil "Lock for Thread Safe Queue ~a" name)))))
+
+(defun make-ts-queue (&optional (name nil name-passed))
+  (apply #'make-instance 'ts-queue (and name-passed (list :name name))))
+
+(defmacro with-ts-queue-lock-held ((queue reason) &body body)
+  `(with-slots (lock name) ,queue
+     (with-lock-held (lock :whostate
+                           (format nil "Thread Safe Queue ~a: ~a"
+                                   name ,reason))
+       ,@body)))
+
+(defun ts-queue-clear (queue)
+  (with-ts-queue-lock-held (queue "clear")
+    (with-slots (head tail)
+        queue
+      (setf head nil
+            tail nil))))
+
+(defun ts-queue->list (queue)
+  "Returns a freshly consed list with the elements in the queue."
+  (with-ts-queue-lock-held (queue "queue->list")
+    (with-slots (head) queue
+      (copy-list head))))
+
+(defun ts-queue-empty? (queue)
+  (with-ts-queue-lock-held (queue "empty?")
+    (with-slots (head) queue
+      (not head))))
+
+(defun ts-enqueue (queue x)
+  "Appends x at the end of the queue and returns x"
+  (with-ts-queue-lock-held (queue "enqueue")
+    (with-slots (head tail) queue
+      (setf tail (last (nconc tail (list x)))
+            head (or head tail))))
+  x)
+
+(defun ts-dequeue (queue)
+  "Removes the first element in the queue and returns it.
+   Returns t as a second value if element is returned.
+   Returns nil as a second value if queue was empty."
+  (with-ts-queue-lock-held (queue "dequeue")
+    (with-slots (head tail) queue
+      (if head
+          (let ((val (car head)))
+            (setf head (cdr head)
+                  tail (and head tail))
+            (values val t))
+          (values nil nil)))))
