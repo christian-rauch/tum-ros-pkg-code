f20cead881d98d960645c159b8e15838b151c9fd
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-07-16 11:43:42 +0000

    Author: Ulrich Friedrich Klank <klank@in.tum.de> Date:   Wed Jul 14 16:57:40 2010 +0200
    commit f740e5a5b4f9fb2b1e9829df9c8695adb6ed4694
    Author: Ulrich Friedrich Klank <klank@in.tum.de>
    Date:   Wed Jul 14 16:57:40 2010 +0200
    
        Swithed output from point to pose
    
    commit 6adbcf47f0e2de6c6b9f778788abfe5130ff6b3f
    Author: Ulrich Friedrich Klank <klank@in.tum.de>
    Date:   Wed Jul 14 14:32:00 2010 +0200
    
        export of SGP standalone
    
    git-commit

--- stacks/grasping/pc_grasp_position/src/grasp_pcd.cpp
+++ stacks/grasping/pc_grasp_position/src/grasp_pcd.cpp
@@ -2,6 +2,8 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud.h>
 #include <SimpleGraspPlanner.h>
+#include <geometry_msgs/Pose.h>
+
 
 using namespace sensor_msgs;
 
@@ -12,12 +14,12 @@ std::string s_output_cloud_topic = "hand_config";
 
 /**
   These parameters depends on the rotation of the robot to the object
-  only the first quadrant is searched, with this value the quadrant can be shifted. 
+  only the first quadrant is searched, with this value the quadrant can be shifted.
   Robots might have different properties, so the quadrant to search might change for
-  higher tables and higher grasps, so there are two values wich can be set on every 
+  higher tables and higher grasps, so there are two values wich can be set on every
   query to sgp.
-  
-  In this example we assume the object to be in the right orientation towards the 
+
+  In this example we assume the object to be in the right orientation towards the
   robot.
 */
 double param_start_top = 0.0;
@@ -27,9 +29,9 @@ double param_start_side = 0.0;
 */
 void cloud_cb (const PointCloudConstPtr& cloud)
 {
-  /* 
-  Cite sgp: 
-    
+  /*
+  Cite sgp:
+
   typedef struct
   {
      double sx; double sxy; double sxz;
@@ -39,19 +41,19 @@ void cloud_cb (const PointCloudConstPtr& cloud)
   CovariancePoint;
   */
   CovariancePoint cov;   /** > grasping target, in order to support obstacles this must be a list */
-  Point3D mean;
+  Point3D mean, a, b, c1,c2,c3;
   mean.x = 0.0;
   mean.y = 0.0;
   mean.z = 0.0;
-  for(int i = 0; i < (*cloud).points.size(); i++)
+  for(size_t i = 0; i < (*cloud).points.size(); i++)
   {
     mean.x += (*cloud).points[i].x;
     mean.y += (*cloud).points[i].y;
     mean.z += (*cloud).points[i].z;
-  }    
+  }
   mean.x /=  (*cloud).points.size();
   mean.y /=  (*cloud).points.size();
-  mean.z /=  (*cloud).points.size();  
+  mean.z /=  (*cloud).points.size();
   cov.sx  = 0.0;
   cov.sxy = 0.0;
   cov.sxz = 0.0;
@@ -61,12 +63,12 @@ void cloud_cb (const PointCloudConstPtr& cloud)
   cov.szx = 0.0;
   cov.szy = 0.0;
   cov.sz  = 0.0;
-  for(int i = 0; i < (*cloud).points.size(); i++)
+  for(size_t i = 0; i < (*cloud).points.size(); i++)
   {
      double tmpx = (*cloud).points[i].x - mean.x;
      double tmpy = (*cloud).points[i].y - mean.y;
      double tmpz = (*cloud).points[i].z - mean.z;
-      
+
      cov.sx  += tmpx*tmpx;
      cov.sxy += tmpx*tmpy;
      cov.sxz += tmpx*tmpz;
@@ -94,10 +96,73 @@ void cloud_cb (const PointCloudConstPtr& cloud)
   /**
     The results can be Transformed r.g. with TransformPoint from the sgp library
     example would be:
-    
+
   */
-  
-  pub.publish(pout);  
+  b.x = 0.0;
+  b.y = 0.0;
+  b.z = 0.0;
+  a.x = 1.0;
+  a.y = 0.0;
+  a.z = 0.0;
+  c1 = TransformPoint(a, b, cfg.alpha, cfg.beta, cfg.delta_max) ;
+  a.x = 0.0;
+  a.y = 1.0;
+  a.z = 0.0;
+  c2 = TransformPoint(a, b, cfg.alpha, cfg.beta, cfg.delta_max) ;
+  a.x = 0.0;
+  a.y = 0.0;
+  a.z = 1.0;
+  c3 = TransformPoint(a, b, cfg.alpha, cfg.beta, cfg.delta_max) ;
+  geometry_msgs::Pose pose;
+  pose.position.x = 0.0;
+  pose.position.y = 0.0;
+  pose.position.z = 0.0;
+
+  /** Transform to quaternion */
+  /** Calculate Trace*/
+  double S, T = 1 + c1.x + c2.y + c3.z;
+
+
+  /* If the trace of the matrix is greater than zero, then
+  perform an "instant" calculation.
+  Important note wrt. rouning errors: */
+  if ( T > 0.00000001 )
+  {
+      S = sqrt(T) * 2;
+      pose.orientation.x = ( c2.z - c3.y ) / S;
+      pose.orientation.y = ( c3.x - c1.z ) / S;
+      pose.orientation.z = ( c1.y - c2.x ) / S;
+      pose.orientation.w = 0.25 * S;
+  }
+  else
+  {
+
+    /*If the trace of the matrix is equal to zero then identify
+    which major diagonal element has the greatest value.
+    Deending on this, calculate the following:*/
+
+    if ( c1.x > c2.y && c1.x > c3.z )  {	// Column 0:
+        S  = sqrt( 1.0 + c1.x - c2.y - c3.z ) * 2;
+        pose.orientation.x = 0.25 * S;
+        pose.orientation.y = (c1.y + c2.x ) / S;
+        pose.orientation.z = (c3.x + c1.z ) / S;
+        pose.orientation.w = (c2.z - c3.y ) / S;
+    } else if ( c2.y > c3.z ) {			// Column 1:
+        S  = sqrt( 1.0 + c2.y - c1.x - c3.z ) * 2;
+        pose.orientation.x = (c1.y + c2.x ) / S;
+        pose.orientation.y = 0.25 * S;
+        pose.orientation.z = (c2.z + c3.y ) / S;
+        pose.orientation.w = (c3.x - c1.z ) / S;
+    } else {						// Column 2:
+        S  = sqrt( 1.0 + c3.z - c1.x - c2.y ) * 2;
+        pose.orientation.x = (c3.x + c1.z ) / S;
+        pose.orientation.y = (c2.z + c3.y ) / S;
+        pose.orientation.z = 0.25 * S;
+        pose.orientation.w = (c1.y - c2.x ) / S;
+    }
+  }
+
+  pub.publish(pose);
 }
 
 #ifndef MAX_HAND_POINTS
@@ -128,9 +193,9 @@ void GetParams(ros::NodeHandle &n)
                ROS_WARN("Ignoring sgp_config entry: Not a point consisting of three real values");
                continue;
             }
-            
-            if( sgp_config_param_value[i][0].getType() !=  XmlRpcValue::TypeDouble || 
-                sgp_config_param_value[i][1].getType() !=  XmlRpcValue::TypeDouble || 
+
+            if( sgp_config_param_value[i][0].getType() !=  XmlRpcValue::TypeDouble ||
+                sgp_config_param_value[i][1].getType() !=  XmlRpcValue::TypeDouble ||
                 sgp_config_param_value[i][2].getType() !=  XmlRpcValue::TypeDouble)
             {
               ROS_WARN("Ignoring sgp_config entry: Not a point consisting of three real values");
@@ -151,7 +216,7 @@ void GetParams(ros::NodeHandle &n)
   {
     XmlRpcValue sgp_config_param_value;
     n.getParam( "sgp_config_param_start_top", sgp_config_param_value );
-    
+
     if(sgp_config_param_value.getType() !=  XmlRpcValue::TypeDouble)
        ROS_WARN("Ignoring sgp_config_paramt_start_top: Not a real value");
     else
@@ -179,11 +244,11 @@ int main(int argc, char* argv[])
   ros::NodeHandle nh("~");
 
   ros::Subscriber cloud_sub = nh.subscribe (s_input_cloud_topic, 1, &cloud_cb);
-  pub = nh.advertise<geometry_msgs::Point32>(s_output_cloud_topic, 1);
+  pub = nh.advertise<geometry_msgs::Pose>(s_output_cloud_topic, 1);
   GetParams(nh);
   while(nh.ok())
-  {    
+  {
     ros::spinOnce ();
-  }                                                    
-  return 0;  
+  }
+  return 0;
 }
