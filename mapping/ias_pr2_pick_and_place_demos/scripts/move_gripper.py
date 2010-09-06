e132f4534399638294f1e62287a7a3c0efa3d371
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-09-06 14:10:42 +0000

    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu> Date:   Mon Sep 6 15:59:46 2010 +0200
    commit b257e2b5bc9502d2be3567149f2972ab81cc3a08
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Mon Sep 6 15:59:20 2010 +0200
    
        description
    
    commit 54e8c1a5960f4d4ce545f9f3cfde09a24a41970b
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Thu Aug 12 19:03:18 2010 +0200
    
        package for ias grasping routines.
        Also includes script for moving the gripper and cleaning the table.
    
    git-commit

---
+++ mapping/ias_pr2_pick_and_place_demos/scripts/move_gripper.py
@@ -0,0 +1,28 @@
+#! /usr/bin/python
+import roslib
+roslib.load_manifest('pr2_mechanism_controllers')
+roslib.load_manifest('actionlib')
+
+import rospy
+import actionlib
+from actionlib_msgs.msg import *
+from pr2_controllers_msgs.msg import *
+
+rospy.init_node('move_the_gripper', anonymous=True)
+
+client = actionlib.SimpleActionClient(
+    'r_gripper_controller/gripper_action', Pr2GripperCommandAction)
+client.wait_for_server()
+
+client.send_goal(Pr2GripperCommandGoal(
+        Pr2GripperCommand(position = 0.09, max_effort = -1)))
+client.wait_for_result()
+
+result = client.get_result()
+did = []
+if client.get_state() != GoalStatus.SUCCEEDED:
+    did.append("failed")
+else:
+    if result.stalled: did.append("stalled")
+    if result.reached_goal: did.append("reached goal")
+print ' and '.join(did)
