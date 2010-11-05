660f18fb3c9293214c0820eb91f274702342e16f
tenorth <tenorth@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-11-05 09:54:12 +0000

    fixed bug in rdfs_computable

--- stacks/knowrob/semweb/prolog/semweb/rdfs_computable.pl
+++ stacks/knowrob/semweb/prolog/semweb/rdfs_computable.pl
@@ -227,16 +227,16 @@ rdfs_computable_instance_of(Instance, Class) :-
 rdfs_computable_instance_of(Instance, Class) :-
   nonvar(Class),
   var(Instance),
-  (findall(I, rdfs_computable_sql_instance_of(I,    Class), Is), member(Instance, Is)) ;
-  (findall(I, rdfs_computable_prolog_instance_of(I, Class), Is), member(Instance, Is)).
+  ((findall(I, rdfs_computable_sql_instance_of(I,    Class), Is), member(Instance, Is)) ;
+   (findall(I, rdfs_computable_prolog_instance_of(I, Class), Is), member(Instance, Is))).
 
 
 % determine classes
 rdfs_computable_instance_of(Instance, Class) :-
   var(Class),
   nonvar(Instance),
-  (findall(C, rdfs_computable_sql_instance_of(Instance,    C), Cs), member(Class, Cs));
-  (findall(C, rdfs_computable_prolog_instance_of(Instance, C), Cs), member(Class, Cs)).
+  ((findall(C, rdfs_computable_sql_instance_of(Instance,    C), Cs), member(Class, Cs));
+   (findall(C, rdfs_computable_prolog_instance_of(Instance, C), Cs), member(Class, Cs))).
 
 
 
