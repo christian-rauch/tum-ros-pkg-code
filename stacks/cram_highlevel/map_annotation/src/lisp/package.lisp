bbbf475be1294ac74b9d003308e6731bc8e0a1bf
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2010-11-17 17:25:34 +0000

    Removed stacks/cram_highlevel
    git-commit

--- stacks/cram_highlevel/map_annotation/src/lisp/package.lisp
+++
@@ -1,9 +0,0 @@
-
-(in-package :cl-user)
-
-(defpackage map-annotation
-    (:use :cl :roslisp :cram-roslisp-common :crs)
-  (:export #:get-closest-annotation
-           #:get-annotated-point
-           #:annotated-point
-           #:get-annotation-names))
--- stacks/cram_highlevel/table_costmap/src/lisp/cost-functions.lisp
+++
@@ -1,8 +0,0 @@
-
-(in-package :table-costmap)
-
-(defun make-table-cost-function (name)
-  (let* ((table-cluster (get-table-cluster name))
-         (mean (3d-vector->mean (mean table-cluster)))
-         (cov (2d-cov (cov table-cluster))))
-    (make-gauss-cost-function mean cov)))
