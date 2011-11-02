1b36ac31e2c2267e5146692b23472c559d3c1d36
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2011-11-02 10:08:47 +0000

    Author: Ingo Kresse <kresse@in.tum.de> Date:   Wed Nov 2 11:05:03 2011 +0100
    git-commit

---
+++ utils/ias_robot_monitor/src/robot_monitor/__init__.py
@@ -0,0 +1,8 @@
+WXVER = ['2.8', '2.9']
+import sys
+import wxversion
+if wxversion.checkInstalled(WXVER):
+  wxversion.select(WXVER)
+else:
+  print >> sys.stderr, "This application requires wxPython version %s"%(WXVER)
+  sys.exit(1)
