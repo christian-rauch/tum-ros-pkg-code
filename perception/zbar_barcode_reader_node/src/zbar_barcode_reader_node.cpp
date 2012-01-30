be5344614e11c9b1dd6777710284a20afb5bb132
moesenle <moesenle@7f8ebb36-02c6-42c3-8f2b-8523c5c70a5a>
2012-01-30 20:53:08 +0000

    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu> Date:   Mon Jan 30 21:49:26 2012 +0100
    commit 7d2243e92e66feb8b1f48eab599384708ffa3da7
    
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Mon Jan 30 21:48:26 2012 +0100
    
        object recognition using barcoo working, major refactoring of the barcode_reader_node.zpp needed
    
    commit 73c93f2f13c5445cfc3955773a323c5e98deada8
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Mon Jan 30 21:47:14 2012 +0100
    
        examples for tinyxml and loading of images with curl and opencv
    
    commit c358c7210ee1c53d032b34118fb84b15c35b9c68
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Sun Aug 29 21:59:21 2010 +0200
    
        created a node for barcode reading,
        	input: image
        	output: type,value
        exchanged c for c++ interface
        tested
        TODO:
        find a n..... that will port to cop:-)
    
    commit 0fd52914acf001cc75df60a84dffc0395a2e769b
    Author: Dejan Pangercic <dejan.pangercic@cs.tum.edu>
    Date:   Sun Aug 29 21:24:21 2010 +0200
    
        c++ implementation of zbar
    git-commit

--- perception/zbar_barcode_reader_node/src/zbar_barcode_reader_node.cpp
+++ perception/zbar_barcode_reader_node/src/zbar_barcode_reader_node.cpp
@@ -9,6 +9,9 @@
 #include <string.h>
 #include "std_msgs/String.h"
 #include <sstream>
+#include "curl/curl.h" 
+#include "tinyxml.h"
+#include <highgui.h>
 
 //Magick++ lib
 #include <Magick++.h>
@@ -18,8 +21,63 @@
 using namespace std;
 using namespace zbar;
 
+ static int writer(char *data, size_t size, size_t nmemb,  
+		      std::string *buffer)  
+    {  
+      // What we will return  
+      int result = 0;  
+  
+      // Is there anything in the buffer?  
+      if (buffer != NULL)  
+	{  
+	  // Append the data to the buffer  
+	  buffer->append(data, size * nmemb);  
+  
+	  // How much did we write?  
+	  result = size * nmemb;  
+	}  
+  
+      return result;  
+    }  
+
+static void* CURL_realloc(void *ptr, size_t size)
+{
+  /* There might be a realloc() out there that doesn't like reallocing
+     NULL pointers, so we take care of it here */
+  if(ptr)
+    return realloc(ptr, size);
+  else
+    return malloc(size);
+}
+
+struct memoryStruct {
+  char *memory;
+  size_t size;
+};
+
+  size_t WriteMemoryCallback
+  (void *ptr, size_t size, size_t nmemb, void *data)
+  {
+    size_t realsize = size * nmemb;
+    struct memoryStruct *mem = (struct memoryStruct *)data;
+    
+    mem->memory = (char *)
+      CURL_realloc(mem->memory, mem->size + realsize + 1);
+    if (mem->memory) {
+      memcpy(&(mem->memory[mem->size]), ptr, realsize);
+      mem->size += realsize;
+      mem->memory[mem->size] = 0;
+    }
+    return realsize;
+  }
+
+
 class BarcodeReaderNode {
 
+  // struct memoryStruct {
+  //   char *memory;
+  //   size_t size;
+  // };
 public:
   BarcodeReaderNode(ros::NodeHandle &n) :
     n_(n), it_(n_)
@@ -35,7 +93,186 @@ public:
   {
 
   }
+  // void* CURL_realloc(void *ptr, size_t size)
+  // {
+  //   /* There might be a realloc() out there that doesn't like reallocing
+  //      NULL pointers, so we take care of it here */
+  //   if(ptr)
+  //     return realloc(ptr, size);
+  //   else
+  //     return malloc(size);
+  // }
 
+  // size_t WriteMemoryCallback
+  // (void *ptr, size_t size, size_t nmemb, void *data)
+  // {
+  //   size_t realsize = size * nmemb;
+  //   struct memoryStruct *mem = (struct memoryStruct *)data;
+    
+  //   mem->memory = (char *)
+  //     CURL_realloc(mem->memory, mem->size + realsize + 1);
+  //   if (mem->memory) {
+  //     memcpy(&(mem->memory[mem->size]), ptr, realsize);
+  //     mem->size += realsize;
+  //     mem->memory[mem->size] = 0;
+  //   }
+  //   return realsize;
+  // }
+    // This is the writer call back function used by curl  
+  // int writer(char *data, size_t size, size_t nmemb,  
+  // 		    std::string *buffer)  
+  // {  
+  //   // What we will return  
+  //   int result = 0;  
+  
+  //   // Is there anything in the buffer?  
+  //   if (buffer != NULL)  
+  //     {  
+  // 	// Append the data to the buffer  
+  // 	buffer->append(data, size * nmemb);  
+  
+  // 	// How much did we write?  
+  // 	result = size * nmemb;  
+  //     }  
+  
+  //   return result;  
+  // }  
+  int getImage(std::string & image)
+  {
+    CURL *curl;       // CURL objects
+    CURLcode res;
+    cv::Mat imgTmp; // image object
+    memoryStruct buffer; // memory buffer
+    
+    curl = curl_easy_init(); // init CURL library object/structure
+    
+    if(curl) 
+      {
+      // set up the write to memory buffer
+      // (buffer starts off empty)
+      buffer.memory = NULL;
+      buffer.size = 0;
+      
+      // (N.B. check this URL still works in browser in case image has moved)
+      curl_easy_setopt(curl, CURLOPT_URL, image.c_str());
+      curl_easy_setopt(curl, CURLOPT_VERBOSE, 1); // tell us what is happening
+      
+      // tell libcurl where to write the image (to a dynamic memory buffer)
+
+      curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+      curl_easy_setopt(curl,CURLOPT_WRITEDATA, (void *) &buffer);
+      
+      // get the image from the specified URL
+      res = curl_easy_perform(curl);
+      // decode memory buffer using OpenCV
+      imgTmp = cv::imdecode(cv::Mat(1, buffer.size, CV_8UC1, buffer.memory), CV_LOAD_IMAGE_UNCHANGED);
+      // display image (if we got / decoded it correctly)
+      
+    
+      if (!(imgTmp.empty()))
+	{
+	  imshow("Image from URL", imgTmp);
+	}
+      cv::waitKey(3);
+      
+      // always cleanup
+
+      curl_easy_cleanup(curl);
+      free(buffer.memory);
+  }
+  return 1;
+  }
+
+  void findElement( TiXmlNode* pParent, std::string & picture)
+  {
+    if ( !pParent ) return;
+    
+    TiXmlNode* pChild;
+    //std::string picture ("Picture Not Found");
+    std::string pstring = pParent->Value();
+    //    std::cerr << "pstring: " << pstring << std::endl;
+    size_t found=pstring.find("picture_high");
+    if (found!=std::string::npos)
+      {
+	ROS_INFO_STREAM("First child: " << pParent->FirstChild()->Value());
+	picture =  pParent->FirstChild()->Value();
+	return;
+      }
+    size_t found2=pstring.find("picture_low");
+    if (found2!=std::string::npos && picture == "")
+      {
+	ROS_INFO_STREAM("First child: " << pParent->FirstChild()->Value());
+	picture =  pParent->FirstChild()->Value();
+	return;
+      }
+    for ( pChild = pParent->FirstChild(); pChild != 0; pChild = pChild->NextSibling()) 
+      {	
+	findElement(pChild, picture);
+	//	std::cerr << "in for " << std::endl;
+      }
+  }
+
+
+  int visBarcooPicture(std::string buffer)
+  {
+    //    std::cerr << "buffer: " << buffer << std::endl;
+    TiXmlDocument doc;
+    doc.Parse((const char*)buffer.c_str(), 0, TIXML_ENCODING_UTF8);
+    doc.SaveFile ("text.xml");
+    std::string picture;
+    findElement (&doc, picture);
+    ROS_INFO_STREAM ("Picture link: " << picture);
+    if (picture == "")
+      return -1;
+    getImage (picture);
+    return 1;
+  }
+
+
+  int callBarcoo(std::string bar_code, std::string & buffer)
+  {
+    char errorBuffer[CURL_ERROR_SIZE];
+    // Our curl objects  
+    CURL *curl;  
+    CURLcode result;  
+    // Create our curl handle  
+    curl = curl_easy_init();  
+    std::string full_url = "http://www.barcoo.com/api/get_product_complete?pi=" + bar_code + "&pins=ean&format=xml&source=ias-tum";
+    ROS_INFO_STREAM("full_url: " << full_url);
+   
+    if (curl)  
+      {  
+	// Now set up all of the curl options  
+	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errorBuffer);  
+	curl_easy_setopt(curl, CURLOPT_URL, full_url.c_str());  
+	curl_easy_setopt(curl, CURLOPT_HEADER, 0);  
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);  
+	//curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &BarcodeReaderNode::writer);  
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writer);  
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);  
+
+	// Attempt to retrieve the remote page  
+	result = curl_easy_perform(curl);  
+
+	// Always cleanup  
+	curl_easy_cleanup(curl);
+
+	// Did we succeed?  
+	if (result == CURLE_OK)  
+	  {  
+	    ROS_INFO_STREAM ("CURLE_OK");
+	    return 1;
+	  }  
+	else  
+	  {  
+	    ROS_INFO_STREAM ( "CURLE_NOT OK");
+	    ROS_ERROR_STREAM ("Error: [" << result << "] - " << errorBuffer);
+	    return -1;
+	  }  
+      }
+    return -1;
+  }
+  
   void imageCallback(const sensor_msgs::ImageConstPtr& msg_ptr)
   {
 
@@ -43,7 +280,7 @@ public:
     ROS_INFO("[BarcodeReaderNode: ] Image received");
     try
       {
-        cv_image = bridge_.imgMsgToCv(msg_ptr, "passthrough");
+        cv_image = bridge_.imgMsgToCv(msg_ptr, "mono8");
       }
     catch (sensor_msgs::CvBridgeException error)
       {
@@ -71,6 +308,7 @@ public:
     int n = scanner.scan(image);
 
     // extract results
+    std::stringstream ss;
     for(Image::SymbolIterator symbol = image.symbol_begin();
         symbol != image.symbol_end();
         ++symbol) 
@@ -80,16 +318,30 @@ public:
                       << " barcode value " << symbol->get_data());
 
       std_msgs::String msg;
-      std::stringstream ss;
-      ss << symbol->get_type_name() << "," << symbol->get_data();
+
+      //ss << symbol->get_type_name() << "," << symbol->get_data();
+      ss << symbol->get_data();
       msg.data = ss.str();
       barcode_pub_.publish(msg);
     }
     if (n == 0)
-      ROS_WARN("Barcode not found");
+      {
+	ROS_WARN("Barcode not found");
+	return;
+      }
 
     if (n < 0)
-      ROS_ERROR("Error occured while finding barcode");
+      {
+	ROS_ERROR("Error occured while finding barcode");
+	return;
+      }
+
+    std::string buffer;
+    if (callBarcoo(ss.str(), buffer) == 1)
+      {
+	//	std::cerr << "buffer after callBarcoo " << buffer << std::endl;
+	visBarcooPicture (buffer);
+      }
     // clean up
     image.set_data(NULL, 0);
 
